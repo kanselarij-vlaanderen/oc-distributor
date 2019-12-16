1871d3ef8b6f0e35cad8360af983faf2b1834773
Michaël Dierick <michael.dierick@redpencil.io>
2019-12-16 17:29:23 +0100

    Adapt to changing user group specs

--- config/access-levels.js
+++ config/access-levels.js
@@ -1,19 +1,11 @@
-export const accessLevels = {
-  'Intern Regering': 'http://kanselarij.vo.data.gift/id/concept/toegangs-niveaus/d335f7e3-aefd-4f93-81a2-1629c2edafa3',
-  'Intern Overheid': 'http://kanselarij.vo.data.gift/id/concept/toegangs-niveaus/abe4c18d-13a9-45f0-8cdd-c493eabbbe29',
-  'Parlement': 'http://kanselarij.vo.data.gift/id/concept/toegangs-niveaus/e3d64f24-4feb-4770-8212-e9e3e23a7d29'
+export const accessLevels = [
+  'http://kanselarij.vo.data.gift/id/concept/toegangs-niveaus/d335f7e3-aefd-4f93-81a2-1629c2edafa3',
+  'http://kanselarij.vo.data.gift/id/concept/toegangs-niveaus/abe4c18d-13a9-45f0-8cdd-c493eabbbe29',
+  'http://kanselarij.vo.data.gift/id/concept/toegangs-niveaus/e3d64f24-4feb-4770-8212-e9e3e23a7d29'
+];
+
+export const accessLevelsByGraph = {
+  'http://mu.semte.ch/graphs/organizations/minister': accessLevels,
+  'http://mu.semte.ch/graphs/organizations/kabinet': accessLevels.slice(1),
+  'http://mu.semte.ch/graphs/organizations/administratie': accessLevels.slice(2)
 };
-
-export const kabinet = [
-  accessLevels['Intern Regering'],
-  accessLevels['Intern Overheid'],
-  accessLevels['Parlement']
-];
-
-export const adviesverlener = [
-  accessLevels['Intern Regering'],
-  accessLevels['Intern Overheid'],
-  accessLevels['Parlement']
-];
-
-export const parlement = [ accessLevels['Parlement'] ];
--- config/graphs.js
+++ config/graphs.js
@@ -1,10 +1,10 @@
 export const graphs = {
   kanselarij: 'http://mu.semte.ch/graphs/organizations/kanselarij',
+  minister: 'http://mu.semte.ch/graphs/organizations/minister',
   kabinet: 'http://mu.semte.ch/graphs/organizations/kabinet',
-  adviesverlener: 'http://mu.semte.ch/graphs/organizations/adviesverlener',
-  parlement: 'http://mu.semte.ch/graphs/organizations/parlement'
+  administratie: 'http://mu.semte.ch/graphs/organizations/administratie'
 };
 
 export const source = graphs.kanselarij;
 
-export const targets = [graphs.kabinet, graphs.adviesverlener, graphs.parlement];
+export const targets = [graphs.minister, graphs.kabinet, graphs.administratie];
